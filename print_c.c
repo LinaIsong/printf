80547f1c5824302e7fc861c8db35fbb34ef66d46
Precious_o <preciousadebola@gmail.com>
2024-07-29 16:30:52 -0700

    Add function that prints a character

---
+++ print_c.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _printf_char - prints character ti standard out
+ * @arg: character to be printed
+ *
+ * Return: Always (success) 0
+ */
+int _printf_char(va_list arg)
+{
+	char str;
+
+	str = va_arg(val, int);
+	_putchar(s);
+
+	return (0);
+}
