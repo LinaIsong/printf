7ba938953c87b7eb1b281baada48f9b5f3b7bd09
LinaIsong <linaudokpoh@gmail.com>
2024-07-31 06:54:58 -0700

    l

--- main.h
+++ main.h
@@ -8,50 +8,30 @@
 #include <limits.h>
 #include <string.h>
 
-#define UNUSED(x) (void)(x)
-#define BUFF_SIZE 1024
-
-/* FLAGS */
-#define F_MINUS 1
-#define F_PLUS 2
-#define F_ZERO 4
-#define F_HASH 8
-#define F_SPACE 16
-
-/* SIZES */
-#define S_LONG 2
-#define S_SHORT 1
-
 /**
- * struct fmt - Struct op
- *
- * @fmt: The format.
- * @fn: The function associated.
- */
-struct fmt
+ * struct format - a new type called format
+ * @spec: Pointer to a format specifier
+ * @fmt: Fointer to a function to print output based
+ * on a format specifier
+ * Description: defines new type called format with its
+ * format specifier and related function
+*/
+
+typedef struct format
 {
-	char fmt;
-	int (*fn)(va_list, char[], int, int, int, int);
-};
-
-
-/**
- * typedef struct fmt fmt_t - Struct op
- *
- * @fmt: The format.
- * @fm_t: The function associated.
- */
-typedef struct fmt fmt_t;
+	char *spec;
+	int (*fmt)();
+} fmt_link;
 
 int _putchar(char c);
 int _printf_char(va_list arg);
-int _printf_string(va_list arg);
+int _printf string(va list arg);
 int _strlen(char *s);
 int _strlenc(const char *s);
-int _printf_percent(void);
-int _printf_ints(va_list args);
-int _printf_dec(va_list args);
+int _printf percent(void);
+int _printf_ints(va list args);
+int printf dec(va list args);
 int _printf(const char *format, ...);
-int _printf_b(va_list val);
+int _printf b(va list val);
 
-#endif /* MAIN_H */
+#endif /* MAIN Η */
